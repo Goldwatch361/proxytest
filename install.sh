e25acb056614416faeb0ac150f63c3be3f4b009d
Goldwatch361 <160144159+Goldwatch361@users.noreply.github.com>
2024-02-15 15:54:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
-
 ##################################
-
 #Proxy Full Install Script by TRAP
-
 ##################################
-
 #colors
-Purp=`tput setaf 5`
+purp=`tput setaf 5`
 reset=`tput sgr0`
-
-echo "Proxy Script by TRAP"
+clear >$(tty)
+echo
+echo
+echo "${purp}Proxy Script by TRAP${reset}"
 echo
 echo
 ##################################
-echo
-echo "Enter Telegram API Key :"
+echo "1. Enter Telegram API Key :"
 read telapikey
+clear >$(tty)
 echo
-echo "Enter Telegram Chat ID :"
+echo
+echo "${purp}Proxy Script by TRAP${reset}"
+echo
+echo
+##################################
+echo "2. Enter Telegram Chat ID :"
 read telchatid
+clear >$(tty)
 echo
 echo
-echo "Your Telegram API Key is : ${Purp}$telapikey ${reset}"
-echo "Your Telegram Chat ID is : ${Purp}$telchatid ${reset}"
+echo "${purp}Proxy Script by TRAP${reset}"
 echo
 echo
+##################################
+echo "Your Telegram API Key is : ${purp}$telapikey ${reset}"
+echo "Your Telegram Chat ID is : ${purp}$telchatid ${reset}"
+echo
+echo
+echo "Is that correct ?"
+echo "press(Y/N) to continue."
+echo
+
+# Wait for the user to press a key
+read -s -n 1 key
+
+# Check which key was pressed
+case $key in
+    y|Y)
+        echo "You pressed 'y'. Continuing..."
+
+echo "$telapikey" > telapikey.txt
+echo "$telchatid" > telchatid.txt
+
+        ;;
+    n|N)
+        echo "You pressed 'n'. Exiting..."
+        exit 1
+        ;;
+    *)
+        echo "Invalid input. Please press 'y' or 'n'."
+        ;;
+esac
