f51f2a8acf89003c6585f453d2dfd3941b826362
Goldwatch361 <160144159+Goldwatch361@users.noreply.github.com>
2024-02-15 15:30:18 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##################################
+
+#Proxy Full Install Script by TRAP
+
+##################################
+
+#colors
+Purp=`tput setaf 5`
+reset=`tput sgr0`
+
+echo "Proxy Script by TRAP"
+echo
+echo
+##################################
+echo
+echo "Enter Telegram API Key :"
+read telapikey
+echo
+echo "Enter Telegram Chat ID :"
+read telchatid
+echo
+echo
+echo "Your Telegram API Key is : ${Purp}$telapikey ${reset}"
+echo "Your Telegram Chat ID is : ${Purp}$telchatid ${reset}"
+echo
+echo
